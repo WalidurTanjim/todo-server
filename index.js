2d0d1d83bd98d3117fbbf6c9f6b99e7cf9be8416
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-02-20 21:35:26 +0600

    mongoDB setup done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,10 +9,37 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express());
 
-app.get('/', (req, res) => {
-    res.send('ToDo server is running.');
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a1a1zbo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('TaskManagement');
+    const usersCollection = db.collection('users');
+    const tasksCollection = db.collection('tasks');
+
+    
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("ToDo server is running.");
+});
 
 app.listen(port, () => {
-    console.log('ToDo server is running with the port:', port);
-})
+  console.log("ToDo server is running with the port:", port);
+});
